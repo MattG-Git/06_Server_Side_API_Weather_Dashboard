b9b7afce8063c6b9da726fd6a983167cdb8acfe7
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-08-01 16:14:20 -0700

    working on local storage and being able to recall past search

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,46 @@ var currentEl = $("#current");
 var daily1El = $("#daily1");
 var searchBarEl = $("#searchBar");
 var srchBtnEl = $("button");
-var rightNow = moment().format('MMM DD, YYYY');
+var rightNow = moment().format('l');
 var latitude = 0;
 var longitude = 0;
 var date = 0;
+var searchHistory = $("#searchHistory");
+var cityList = []; 
 
+getStoredInput(); 
+console.log(cityList); 
 
-//fetch(currentUrl);
+function getStoredInput() {
+    var citiesSearched = JSON.parse(localStorage.getItem("cityList"));
+
+    if (citiesSearched !== null) {
+        cityList = citiesSearched;
+    }
+    displaySearch();
+};
+
+function displaySearch() {
+    searchHistory.empty();
+    for (var i =0; i < cityList.length; i++) {
+        var cities = cityList[i];
+
+        var list = $("<button>").text(cities);
+        //list.attr("id",`previousSearch" ${[i]}`);
+        list.attr("id", cities); 
+        list.attr("city-info", cities);
+        list.attr("class", "pastSearchList");
+        console.log(list); 
+        console.log(list.attr("id"))
+        searchHistory.prepend(list);
+    };
+};
 
 function fetchLatLong(){
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
     .then(function(resp) {return resp.json() })
     .then(function(data) {
-        console.log(data);
+        //console.log(data);
         latitude = data[0].lat;
         longitude = data[0].lon;
         fetchCurrentWeather(latitude, longitude);
@@ -27,13 +54,13 @@ function fetchLatLong(){
 };
 
 function fetchCurrentWeather(latitude, longitude) {
-    console.log(latitude, longitude)
+    //console.log(latitude, longitude)
     fetch("http://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey) 
     // the following code converts to json
     .then(function(resp) { return resp.json() }) 
     .then(function(data) {
     currentWeather(data);
-    console.log(data);
+    //console.log(data);
     })
     .catch(function() {
     // catch any errors
@@ -41,14 +68,13 @@ function fetchCurrentWeather(latitude, longitude) {
   };
 
   function fetchForecast() {
-    console.log(city);
+    //console.log(city);
     fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey)
     // the following code converts to json
     .then(function(resp) { return resp.json() }) 
     .then(function(data) {
-    console.log(data);
+    //console.log(data);
     dailyWeather(data);
-
     })
     .catch(function() {
       // catch any errors
@@ -69,7 +95,7 @@ function currentWeather(d) {
     var humidCurrent = $("<p>").text(`Humidity: ${d.current.humidity} %`);
     var uvCurrent = $("<p>").text(`UV Index: ${d.current.uvi}`);
     var uv = parseFloat(d.current.uvi);
-    console.log(uv);
+    //console.log(uv);
     //console.log(tempF);
 
     //this makes the background on the UV index change colors depending on if it's above 6 or not
@@ -79,6 +105,7 @@ function currentWeather(d) {
         uvCurrent.css("background-color", "red"); 
     }; 
 
+    //this appends to the html doc
     currentEl.append(
         currentHeader,
         date,
@@ -93,11 +120,11 @@ function currentWeather(d) {
 //this function displays the 5 day weather forecast information
 function dailyWeather(d) {
     $("#daily1").empty();
-    console.log(d)
+    //console.log(d)
     for (let i =0; i < d.list.length; i = i + 8){
-        console.log(i,d.list[i])
+        //console.log(i,d.list[i])
         date++;
-        var dailyOne = moment().add(date, 'd');
+        var dailyOne = moment().add(date, 'd').format("l");
         var dailyIconCode = d.list[i].weather[0].icon;
         var dailyIconUrl = "http://openweathermap.org/img/wn/" + dailyIconCode + ".png"
         var dailyWeatherIcon = $("<img>").attr("src",dailyIconUrl);
@@ -107,7 +134,7 @@ function dailyWeather(d) {
         var dailyhumid = $("<p>").text(`Humidity: ${d.list[i].main.humidity} %`)
         var cardForecast = $("<div>")
 
-        
+        //this appends to the html doc
         cardForecast.append(
             dailyOne,
             dailyWeatherIcon,
@@ -115,10 +142,11 @@ function dailyWeather(d) {
             dailyWind,
             dailyhumid
         );
+
         //this utilizes bootstrap's cards to inline style the 5 day forecast
         $("#daily1").append(
             `<div class="card" style="width: 9rem;">
-            <h5 class="card-title">${dailyOne}</h5>
+                <h5 class="card-title text-center">${dailyOne}</h5>
                 
             <div class="card-body">
                 <img class="mx-auto d-block" src="${dailyIconUrl}">
@@ -134,9 +162,29 @@ function dailyWeather(d) {
 function userSearch() {
     var cityInput = searchBarEl.val().trim();
     city = cityInput;
+    
+   function storedSearch() {
+        cityList.push(city)
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+        console.log(localStorage, cityList);
+    };
+
+    storedSearch(); 
     fetchLatLong();
     fetchCurrentWeather();
     fetchForecast();
+    getStoredInput();
 };
+
+function performPrevious() { 
+    city = button; 
+    console.log(city)   
+    fetchLatLong();
+    fetchCurrentWeather();
+    fetchForecast();
+    getStoredInput();
+}
+
+$(".pastSearchList").on("click", performPrevious);
 
 srchBtnEl.on("click", userSearch);
